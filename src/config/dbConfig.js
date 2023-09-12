cd467f655d9212bb3b271f566894944df88142d1
Havochalo <officialmail85@gmail.com>
2023-09-12 15:01:43 +1000

    Env fixed

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const dbConnection = () => {
   try {
-    const connect = mongoose.connect(
-      "mongodb+srv://ecomm_admin:ecomm_admin@cluster0.hapt5vb.mongodb.net/ecomm_database"
-    );
+    const connect = mongoose.connect(process.env.MONGOURL);
     connect
       ? console.log("DB connection success")
       : console.log("Cannot connect to DB");
