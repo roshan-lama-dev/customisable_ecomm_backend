6b93c4b61b385b8caa0a4bf94060eebdfe3eecd9
roshan-lama-dev <officialmail85@gmail.com>
2023-09-15 11:43:42 +1000

    Admin routes completed

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { verifyToken, verifyTokenAndAdmin } from "../utilis/verifyToken.js";
+import {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorisation,
+} from "../utilis/verifyToken.js";
 import CryptoJs from "crypto-js";
 import User from "../model/User.js";
 const router = express.Router();
 
-router.put("/:id", verifyTokenAndAdmin, async (req, res, next) => {
+router.put("/:id", verifyTokenAndAuthorisation, async (req, res, next) => {
   if (req.body.password) {
     req.body.password = CryptoJs.AES.encrypt(
       req.body.password,
@@ -13,18 +17,97 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res, next) => {
   }
 
   try {
-    const _id = req.params.id
+    const _id = req.params.id;
     const updatedUser = await User.findByIdAndUpdate(
-      _id ,
+      _id,
       {
         $set: req.body,
       },
       { new: true }
     );
     res.status(200).json({
-        status:"success",
-        message:updatedUser
-    })
+      status: "success",
+      message: updatedUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete
+
+router.delete("/:id", verifyTokenAndAuthorisation, async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+      message: "Deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get singleUser
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json({
+      status: "success",
+      message: others,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get all Users
+
+router.get("/", verifyTokenAndAdmin, async (req, res, next) => {
+  const query = req.query.new;
+
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+
+    res.status(200).json({
+      status: "success",
+      message: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get user stats
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res, next) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      message: data,
+    });
   } catch (error) {
     next(error);
   }
--- src/utilis/verifyToken.js
+++ src/utilis/verifyToken.js
@@ -3,8 +3,7 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
-
-    const token = authHeader.split(" ")[1]
+    const token = authHeader.split(" ")[1];
     // console.log(token)
     jwt.verify(token, process.env.JWT_KEY, (err, user) => {
       if (err)
@@ -13,7 +12,6 @@ export const verifyToken = (req, res, next) => {
           message: "Token is not valid",
         });
 
-        
       req.user = user;
       next();
     });
@@ -25,7 +23,7 @@ export const verifyToken = (req, res, next) => {
   }
 };
 
-export const verifyTokenAndAdmin = (req, res, next) => {
+export const verifyTokenAndAuthorisation = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
@@ -33,6 +31,20 @@ export const verifyTokenAndAdmin = (req, res, next) => {
       res.status(403).json({
         status: "error",
         message: "Invalid Token",
+      });
+    }
+  });
+};
+
+// we need this middleware because when getting the user  we will not send the userID as a params.
+export const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json({
+        status: "error",
+        message: "You are not autorised to perform the action",
       });
     }
   });
